cb5404b4cdbd997322c5122d3d2d6673d46f7514
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-05-02 16:08:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const navbar = document.getElementById('navbar');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
-    //Checking page is scrolled more than 50 px veritcally then adding class
     navbar.classList.add('scrolled');
   } else {
-    
     navbar.classList.remove('scrolled');
   }
 });
